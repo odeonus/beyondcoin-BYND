cb7e65439b73e38503d4610643c19a25c0eb9ff3
Kristian Kramer <kristian@beyonddata.llc>
2019-10-19 16:56:30 -0600

    Update Beyondcoin denomination descriptors

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -42,7 +42,7 @@ QString BitcoinUnits::name(int unit)
     {
     case BTC: return QString("BYND");
     case mBTC: return QString("mBYND");
-    case uBTC: return QString("uBYND");
+    case uBTC: return QString("μBYND");
     default: return QString("???");
     }
 }
@@ -52,8 +52,8 @@ QString BitcoinUnits::description(int unit)
     switch(unit)
     {
     case BTC: return QString("Beyondcoins");
-    case mBTC: return QString("mBYND (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("uBYND (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case mBTC: return QString("Milli-Beyondcoins (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Micro-Beyondcoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
