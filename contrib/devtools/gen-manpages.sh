a0ae16ac4ca1bf0cc46d1003132aae4e484e5e61
Kristian Kramer <kristian@beyonddata.llc>
2019-12-07 13:33:09 -0700

    ltc -> bynd

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -12,7 +12,7 @@ BEYONDCOINQT=${BEYONDCOINQT:-$SRCDIR/qt/beyondcoin-qt}
 [ ! -x $BEYONDCOIND ] && echo "$BEYONDCOIND not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-LTCVER=($($BEYONDCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+BYNDVER=($($BEYONDCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
 # This gets autodetected fine for bitcoind if --version-string is not set,
@@ -22,8 +22,8 @@ $BEYONDCOIND --version | sed -n '1!p' >> footer.h2m
 
 for cmd in $BEYONDCOIND $BEYONDCOINCLI $BEYONDCOINTX $BEYONDCOINQT; do
   cmdname="${cmd##*/}"
-  help2man -N --version-string=${LTCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
-  sed -i "s/\\\-${LTCVER[1]}//g" ${MANDIR}/${cmdname}.1
+  help2man -N --version-string=${BYNDVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
+  sed -i "s/\\\-${BYNDVER[1]}//g" ${MANDIR}/${cmdname}.1
 done
 
 rm -f footer.h2m
