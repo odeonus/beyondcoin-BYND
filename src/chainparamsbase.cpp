d81ecc81bb3883b2c2a898179880d6d1c700b846
Kristian Kramer <kristian@beyonddata.llc>
2019-12-07 13:15:43 -0700

    Update RPC ports

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -33,7 +33,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 9332;
+        nRPCPort = 10332;
     }
 };
 
@@ -45,7 +45,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 19332;
+        nRPCPort = 14332;
         strDataDir = "testnet4";
     }
 };
@@ -58,7 +58,7 @@ class CBaseRegTestParams : public CBaseChainParams
 public:
     CBaseRegTestParams()
     {
-        nRPCPort = 19443;
+        nRPCPort = 11332;
         strDataDir = "regtest";
     }
 };
