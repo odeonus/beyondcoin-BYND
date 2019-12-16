81528543c293d37f3587d96e9a3506adeddc0018
Kristian Kramer <kristian@beyonddata.llc>
2019-12-15 20:16:57 -0700

    Update website URL

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -47,7 +47,7 @@ static const int MAX_URI_LENGTH = 255;
 #define SPINNER_FRAMES 36
 
 #define QAPP_ORG_NAME "Beyondcoin"
-#define QAPP_ORG_DOMAIN "beyondcoin.org"
+#define QAPP_ORG_DOMAIN "beyondcoin.io"
 #define QAPP_APP_NAME_DEFAULT "Beyondcoin-Qt"
 #define QAPP_APP_NAME_TESTNET "Beyondcoin-Qt-testnet"
 
