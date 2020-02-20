39fddeabdaf696318550d9aec3c1caa88966a1e8
Kristian Kramer <kristian@beyonddata.llc>
2020-02-19 21:07:19 -0700

    GUI: Add option to select address gen type

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -6,6 +6,7 @@
 #define BITCOIN_QT_OPTIONSMODEL_H
 
 #include <amount.h>
+#include <wallet/wallet.h>
 
 #include <QAbstractListModel>
 
@@ -48,6 +49,9 @@ public:
         ThreadsScriptVerif,     // int
         DatabaseCache,          // int
         SpendZeroConfChange,    // bool
+        AddressTypeLegacy,      // bool
+        AddressTypeP2SHSegWit,  // bool
+        AddressTypeBech32,      // bool
         Listen,                 // bool
         OptionIDRowCount,
     };
