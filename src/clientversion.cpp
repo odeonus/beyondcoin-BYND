6eb0ce97996c1f307fdcb74d65b38105de087a80
Kristian Kramer <kristian@beyonddata.llc>
2020-02-22 05:09:32 +0000

    Update client version

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -44,8 +44,8 @@ const std::string CLIENT_NAME("BeyondcoinCore");
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives.
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "412efc80e773"
-#define GIT_COMMIT_DATE "Fri Feb 21 02:32:28 2020 +0000"
+#define GIT_COMMIT_ID "222ccd2ea361"
+#define GIT_COMMIT_DATE "Sat Feb 22 05:07:36 2020 +0000"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
